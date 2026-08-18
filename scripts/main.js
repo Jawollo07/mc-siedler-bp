f021e1f17153d899fd4e367b06b3d32a5b6192ab
Jawollo07 <strengerjannik@gmail.com>
2026-08-18 19:32:41 +0200

    Changed File Structure

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,18 @@
 import { system } from "@minecraft/server";
 
-// Alle Module statisch laden, damit Event- und Dynamic-Property-Handler
-// rechtzeitig vor der Weltinitialisierung registriert werden.
 import "./teams/index.js";
 import "./teams/chat.js";
+
 import "./taxes/time_watcher.js";
+
 import "./claims/index.js";
 import "./claims/protection.js";
 import "./claims/display.js";
+
 import "./monster/index.js";
-import "./monster/commads.js";
 import "./monster/pillager_squads.js";
 import "./monster/outpost_raids.js";
+
 import "./essentials/index.js";
 
 console.info("§8----------------------------------------");
