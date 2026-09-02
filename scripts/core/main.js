ecca38fb27c0cef60f294d018ca9816bd6aa257d
Jannik Strenger <jannik@mcjj.de>
2026-09-02 19:45:46 +0200

    Fixed main.js

--- scripts/core/main.js
+++ scripts/core/main.js
@@ -59,7 +59,7 @@ import ".././essentials/player_stats.js";
 // Soldier system
 // -----------------------------------------------------------------------------
 import ".././soldier/index.js";
-import "../tests/equipment.js";
+
 const VERSION = version;
 const MODULE_COUNT = 18;
 const STARTUP_DELAY = 20;
