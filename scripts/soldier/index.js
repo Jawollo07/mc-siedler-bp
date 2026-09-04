56430ce0e77079bd3b91942391c877052114fe5b
Jawollo07 <jannik@mcjj.de>
2026-09-04 21:38:24 +0200

    fix(soldier): activate melee combat range fix

--- scripts/soldier/index.js
+++ scripts/soldier/index.js
@@ -4,7 +4,9 @@ import "./commands.js";
 import "./ui.js";
 import "./level.js";
 import { startSoldierAI } from "./ai.js";
+import { startCombatRangeFix } from "./combat_range.js";
 
 startSoldierAI();
+startCombatRangeFix();
 
 console.log("[SOLDIER] Loaded");
