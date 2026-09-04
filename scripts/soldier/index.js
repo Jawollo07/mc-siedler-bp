fb819159af35a3ea6b32343b96b4750c2d4e05b6
Jawollo07 <jannik@mcjj.de>
2026-09-04 21:53:56 +0200

    feat(soldier): start ranged archer AI

--- scripts/soldier/index.js
+++ scripts/soldier/index.js
@@ -5,8 +5,10 @@ import "./ui.js";
 import "./level.js";
 import { startSoldierAI } from "./ai.js";
 import { startCombatRangeFix } from "./combat_range.js";
+import { startRangedAI } from "./ranged_ai.js";
 
 startSoldierAI();
 startCombatRangeFix();
+startRangedAI();
 
 console.log("[SOLDIER] Loaded");
