f7c2cd4e60260c455a567c6281443d0f58cb4d9c
Jawollo07 <jannik@mcjj.de>
2026-09-08 20:10:21 +0200

    feat(soldier): load persistent soldier registry

--- scripts/soldier/index.js
+++ scripts/soldier/index.js
@@ -4,6 +4,7 @@ import "./cavalry.js";
 import "./commands.js";
 import "./ui.js";
 import "./level.js";
+import "./registry.js";
 import { startSoldierAI } from "./ai.js";
 import { startCombatRangeFix } from "./combat_range.js";
 import { startRangedAI } from "./ranged_ai.js";
